1e419d22db6f46c622fa3c052a0413915097d864
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-03-22 19:26:54 +0800

    remove path /?ed=2048

--- _worker.js
+++ _worker.js
@@ -847,7 +847,7 @@ function generateUUID() {
 async function getVLESSConfig(userID, hostName, sub, userAgent, RproxyIP) {
 	// 如果sub为空，则显示原始内容
 	if (!sub || sub === '') {
-		const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`;
+		const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2Ffireinrain#${hostName}`;
   
 		return `
 	################################################################
@@ -869,14 +869,14 @@ async function getVLESSConfig(userID, hostName, sub, userAgent, RproxyIP) {
 	  sni: ${hostName}
 	  client-fingerprint: chrome
 	  ws-opts:
-	    path: "/?ed=2048"
+	    path: "/fireinrain"
 	    headers:
 		  host: ${hostName}
 	---------------------------------------------------------------
 	################################################################
 	`;
 	} else if (sub && userAgent.includes('mozilla') && !userAgent.includes('linux x86')) {
-		const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`;
+		const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2Ffireinrain#${hostName}`;
 	
 		return `
 	################################################################
@@ -903,7 +903,7 @@ async function getVLESSConfig(userID, hostName, sub, userAgent, RproxyIP) {
 	  sni: ${hostName}
 	  client-fingerprint: chrome
 	  ws-opts:
-		path: "/?ed=2048"
+		path: "/fireinrain"
 		headers:
 		  host: ${hostName}
 	---------------------------------------------------------------
