ef316ef641e03c4003820a342837270eccaac74d
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-04-02 19:22:22 +0800

    update

--- _worker.js
+++ _worker.js
@@ -869,7 +869,7 @@ function generateUUID() {
 async function getVLESSConfig(userID, hostName, sub, userAgent, RproxyIP) {
     // 如果sub为空，则显示原始内容
     if (!sub || sub === '') {
-        const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`;
+        const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2Filovekleecloud%3Fed%3D2048#${hostName}`;
 
         return `
 	################################################################
@@ -891,14 +891,14 @@ async function getVLESSConfig(userID, hostName, sub, userAgent, RproxyIP) {
 	  sni: ${hostName}
 	  client-fingerprint: chrome
 	  ws-opts:
-	    path: "/?ed=2048"
+	    path: "/ilovekleecloud?ed=2048"
 	    headers:
 		  host: ${hostName}
 	---------------------------------------------------------------
 	################################################################
 	`;
     } else if (sub && userAgent.includes('mozilla') && !userAgent.includes('linux x86')) {
-        const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`;
+        const vlessMain = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2Filovekleecloud%3Fed%3D2048#${hostName}`;
 
         return `
 	################################################################
@@ -925,7 +925,7 @@ async function getVLESSConfig(userID, hostName, sub, userAgent, RproxyIP) {
 	  sni: ${hostName}
 	  client-fingerprint: chrome
 	  ws-opts:
-		path: "/?ed=2048"
+		path: "/ilovekleecloud?ed=2048"
 		headers:
 		  host: ${hostName}
 	---------------------------------------------------------------
