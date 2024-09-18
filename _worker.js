b15cefb5086774d93af043a3df6d6aea28f0bcef
sanshengshiqd <140888084+sanshengshiqd@users.noreply.github.com>
2024-09-18 10:00:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -38,7 +38,7 @@ let addresses = [
 	//当sub为空时启用本地优选域名/优选IP，若不带端口号 TLS默认端口为443，#号后为备注别名
 	/*
 	'Join.my.Telegram.channel.CMLiussss.to.unlock.more.premium.nodes.cf.090227.xyz#加入我的频道t.me/CMLiussss解锁更多优选节点',
-	'visa.cn:443',
+	'086181.xyz:443',
 	'www.visa.com:8443',
 	'cis.visa.com:2053',
 	'africa.visa.com:2083',
